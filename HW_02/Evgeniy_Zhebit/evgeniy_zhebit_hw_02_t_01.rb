00ef9f01743b16506511c2c46214ab9e36e0175f
JackyZhebit <Jackyzhebit@gmail.com>
2019-04-02 22:16:10 +0200

    bug fix

--- HW_02/Evgeniy_Zhebit/evgeniy_zhebit_hw_02_t_01.rb
+++ HW_02/Evgeniy_Zhebit/evgeniy_zhebit_hw_02_t_01.rb
@@ -1,13 +1,18 @@
-puts 'Enter the value of hours hand (from 0 to 12)'
-a = gets.chomp.to_i
-puts 'Enter the value of minutes hand (from 0 to 59)'
-b = gets.chomp.to_i
-puts 'the angle between the hours and the minutes is:'
-# If the hour's hand don't moove
-# puts (a * 30 - b * 6).abs
-# If the hour's hand is mooving
-if a * 60 < b * 12
-  puts(0.5 * (60 * a + b - 12 * b)).abs
+puts'Enter the value of hours hand (from 0 to 12)'
+h = gets.chomp.to_i
+if h < 0 || h > 12
+  puts('Invalid hours')
+  exit(-1)
+end
+puts'Enter the value of minutes hand (from 0 to 59)'
+m = gets.chomp.to_i
+if m < 0 || m > 59
+  puts('Invalid minutes')
+  exit(-1)
+end
+puts 'The angle between the hours and the minutes hands is:'
+if h * 60 < m * 12
+  puts((0.5 * (60 * h + m - 12 * m)).abs)
 else
-  puts(0.5 * (60 * a - b - 12 * b)).abs
+  puts((0.5 * (60 * h - m - 12 * m)).abs)
 end
